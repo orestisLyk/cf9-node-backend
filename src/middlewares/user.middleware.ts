9f7d18995b9b615e36b374bdfdb756e55fe711bb
orestisLyk <orestis.lyk@outlook.com>
2026-04-20 16:23:13 +0300

    feat: Add role-based access control to user middleware

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -1,9 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-import User from "../models/user.model";
-
+import { IRole } from "../models/role.model";
 export const hasReaderRole = (req: Request, res: Response, next: NextFunction) => {
     try{
-
+        const checkReaderRole = req.user.roles.some( (r:IRole) => r.role === "reader" && r.active);
+        if(!checkReaderRole) {
+            return res.status(403).json({ message: "Forbidden" });
+        }
+        next();
     } catch (error) {
         res.status(401).json({ message: "Invalid or Expired token" });
     }
