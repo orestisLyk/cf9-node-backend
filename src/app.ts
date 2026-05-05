b5c5de1e0898b0ea13e25516ee2e4186aeda4b6a
orestisLyk <orestis.lyk@outlook.com>
2026-05-05 15:51:12 +0300

    feat: Add CORS support to allow requests from Angular frontend

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,15 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
 import { setupSwagger } from './swagger';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:4200']
+}));
 
 setupSwagger(app);
 app.use(express.json());
